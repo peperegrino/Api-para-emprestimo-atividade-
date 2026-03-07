9405cfd6ad8c504153220fad59111b89ca47e370
FELIPE <luizfalmeidalima38@gmail.com>
2026-03-06 22:23:28 -0300

    finalizei o CRUD do loan

--- src/main/java/senai/com/emprestimoapi/service/LoanService.java
+++ src/main/java/senai/com/emprestimoapi/service/LoanService.java
@@ -1,8 +1,16 @@
 package senai.com.emprestimoapi.service;
 
 import org.springframework.stereotype.Service;
+import senai.com.emprestimoapi.DTOS.LoanRequestDTO;
+import senai.com.emprestimoapi.DTOS.LoanResponseDTO;
+import senai.com.emprestimoapi.DTOS.UserRequestDTO;
 import senai.com.emprestimoapi.Repositories.LoanRepository;
 import senai.com.emprestimoapi.Repositories.UserRepository;
+import senai.com.emprestimoapi.entities.Loan;
+import senai.com.emprestimoapi.entities.User;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LoanService {
@@ -14,4 +22,32 @@ public class LoanService {
     }
 
 
+
+    public LoanResponseDTO createLoan(LoanRequestDTO dto) {
+  Loan loan = new Loan(dto.getLoanDate(), dto.getDevolutionDate());
+  loanRepository.save(loan);
+  return new LoanResponseDTO(loan);
+
+     }
+
+     public List<Loan> showLoans() {
+         return loanRepository.findAll();
+    }
+
+    public void deleteLoan(Long id) {
+        loanRepository.deleteById(id);
+    }
+
+    public Loan updateLoan(Long id , LoanRequestDTO dto) {
+        Loan l = loanRepository.findById(id).orElse(null);
+
+        if (l != null) {
+            l.setLoanDate(dto.getLoanDate());
+            l.setDevolutionDate(dto.getDevolutionDate());
+            return loanRepository.save(l);
+        } else {
+            return null;
+        }
+    }
 }
+
